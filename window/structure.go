731c3611899a6d2461ece57838ae022d2ffc9707
zaneway1108@gmail.com <zaneway1108@gmail.com>
2024-08-17 23:22:15 +0800

    完善清除功能

--- window/structure.go
+++ window/structure.go
@@ -13,13 +13,14 @@ import (
 // 构造解析证书核心图形模块
 func Structure() *fyne.Container {
 	structure := container.NewVBox()
+	detail := container.NewVBox()
 	inputCertEntry := buildInputCertEntry("please input base64/hex cert")
 	certDetail := make(map[string]string)
 	inputCertEntry.Text = "MIICETCCAbWgAwIBAgINKl81oFaaablKOp0YTjAMBggqgRzPVQGDdQUAMGExCzAJBgNVBAYMAkNOMQ0wCwYDVQQKDARCSkNBMSUwIwYDVQQLDBxCSkNBIEFueXdyaXRlIFRydXN0IFNlcnZpY2VzMRwwGgYDVQQDDBNUcnVzdC1TaWduIFNNMiBDQS0xMB4XDTIwMDgxMzIwMTkzNFoXDTIwMTAyNDE1NTk1OVowHjELMAkGA1UEBgwCQ04xDzANBgNVBAMMBuWGr+i9rDBZMBMGByqGSM49AgEGCCqBHM9VAYItA0IABAIF97Sqq0Rv616L2PjFP3xt16QGJLmi+W8Ht+NLHiXntgUey0Nz+ZVnSUKUMzkKuGTikY3h2v7la20b6lpKo8WjgZIwgY8wCwYDVR0PBAQDAgbAMB0GA1UdDgQWBBSxiaS6z4Uguz3MepS2zblkuAF/LTAfBgNVHSMEGDAWgBTMZyRCGsP4rSes0vLlhIEf6cUvrjBABgNVHSAEOTA3MDUGCSqBHIbvMgICAjAoMCYGCCsGAQUFBwIBFhpodHRwOi8vd3d3LmJqY2Eub3JnLmNuL2NwczAMBggqgRzPVQGDdQUAA0gAMEUCIG6n6PG0BOK1EdFcvetQlC+9QhpsTuTui2wkeqWiPKYWAiEAvqR8Z+tSiYR5DIs7SyHJPWZ+sa8brtQL/1jURvHGxU8="
 	//确认按钮
 	confirm := buildButton("确认", func() {
 		inputCert := inputCertEntry.Text
-
+		detail.RemoveAll()
 		decodeCert, err := base64.StdEncoding.DecodeString(inputCert)
 		if err != nil {
 			fyne.LogError("解析请求错误", err)
@@ -40,7 +41,7 @@ func Structure() *fyne.Container {
 		certDetail["PublicKey"] = base64.StdEncoding.EncodeToString(certificate.RawSubjectPublicKeyInfo)
 
 		//添加证书详情
-		showCertificateDetail(certDetail, structure)
+		showCertificateDetail(certDetail, detail)
 	})
 	//清除按钮
 	clear := buildButton("清除", func() {
@@ -52,7 +53,7 @@ func Structure() *fyne.Container {
 	allButton := container.New(layout.NewGridLayout(2), confirm, clear)
 	structure.Add(inputCertEntry)
 	structure.Add(allButton)
-
+	structure.Add(detail)
 	return structure
 }
 
