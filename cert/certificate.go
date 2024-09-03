1a47290ec4df0cad6577da8b50a8faba8f18c233
zaneway1108@gmail.com <zaneway1108@gmail.com>
2024-09-03 22:25:04 +0800

    补充密钥用途解析

--- cert/certificate.go
+++ cert/certificate.go
@@ -5,10 +5,35 @@ import (
 	gm "github.com/tjfoc/gmsm/x509"
 )
 
+var KeyUsages = make(map[gm.KeyUsage]string)
+
+func init() {
+	KeyUsages[gm.KeyUsageDigitalSignature] = "DigitalSignature(数字签名)"
+	KeyUsages[gm.KeyUsageKeyEncipherment] = "KeyEncipherment(密钥加密)"
+	KeyUsages[gm.KeyUsageDataEncipherment] = "DataEncipherment(数据加密)"
+	KeyUsages[gm.KeyUsageKeyAgreement] = "KeyAgreement(密钥协商)"
+	KeyUsages[gm.KeyUsageCertSign] = "CertSign(证书签发)"
+	KeyUsages[gm.KeyUsageCRLSign] = "CRLSign(CRL签发)"
+	KeyUsages[gm.KeyUsageEncipherOnly] = "EncipherOnly(仅加密)"
+	KeyUsages[gm.KeyUsageDecipherOnly] = "DecipherOnly(仅解密)"
+}
+
 func ParseCertificate(cert []byte) (*gm.Certificate, error) {
 	certificate, err := gm.ParseCertificate(cert)
 	if err != nil {
 		fmt.Println(err)
 	}
 	return certificate, nil
+}
+
+// 根据密钥用途解析具体值
+func ParseKeyUsage(usage gm.KeyUsage) string {
+	var result string
+	for key, value := range KeyUsages {
+		if key&usage != 0 {
+			result += value
+			result += " "
+		}
+	}
+	return result
 }
