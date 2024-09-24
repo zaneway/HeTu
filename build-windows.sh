1af22c5b1f3039f7c6aa61bcf7cff0118b5c6f3e
zaneway1108@gmail.com <zaneway1108@gmail.com>
2024-09-24 19:38:09 +0800

    修改脚本

--- build-windows.sh
+++ build-windows.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -o CertViewer.exe
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -ldflags "-H windowsgui" -o CertViewer.exe
 
 echo over
