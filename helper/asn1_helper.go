65ce3383e51e4ae77b07e7e893eaf957e0dddb0c
zaneway <zaneway1108@gmail.com>
2024-10-05 22:29:26 +0800

    Add a name to a complex structure

--- helper/asn1_helper.go
+++ helper/asn1_helper.go
@@ -11,7 +11,7 @@ import (
 
 // 定义tag和名称的映射关系
 var TagToName = map[int]string{
-	0:  "EOC",
+	0:  "",
 	1:  "BOOLEAN",
 	2:  "INTEGER",
 	3:  "BIT STRING",
@@ -32,7 +32,15 @@ var TagToName = map[int]string{
 	30: "TagBMPString",
 }
 
+var ClassToNum = map[int]int{
+	0: 0,
+	1: 64,  //0x40,第7位为1
+	2: 128, //0x80,第8位为1
+	3: 192, //0xc0,第8位和第7位为1
+}
+
 type ASN1Node struct {
+	//this Tag is real Number in asn1
 	Tag, Class, Length int
 	Children           []*ASN1Node
 	Content, SHA256    string
@@ -50,6 +58,12 @@ func ParseAsn1(data []byte, resultMap map[string]ASN1Node) ASN1Node {
 	//tag to hex
 	//todo 后期转为asn1的tag类型描述,并对应string\oid等转换
 	thisNode.Tag = node.Tag
+
+	if node.IsCompound {
+		//如果是结构类型,则第6位为1
+		thisNode.Tag = node.Tag + 32
+	}
+	thisNode.Tag += ClassToNum[node.Class]
 	thisNode.Class = node.Class
 	//len
 	thisNode.Length = len(node.FullBytes)
--- window/asn1_viewer.go
+++ window/asn1_viewer.go
@@ -17,7 +17,8 @@ import (
 func buildAccordion(node ASN1Node, level int) *widget.AccordionItem {
 	// 缩进根据层级来决定
 	indentation := fyne.NewSize(float32(level*30), 0) // 通过level决定缩进量
-	value := TagToName[node.Tag]
+
+	value := getRealTagName(node.Tag) + hex
 	// 节点的内容展示
 	content := widget.NewLabel(fmt.Sprintf("%s :", value))
 	content.Resize(fyne.NewSize(600, content.MinSize().Height))
@@ -54,7 +55,7 @@ func Asn1Structure() *fyne.Container {
 	// 创建输入框，供用户输入Base64数据
 	input := widget.NewMultiLineEntry()
 	input.SetPlaceHolder("Please input base64/hex cert")
-	//input.Text = "MIICETCCAbWgAwIBAgINKl81oFaaablKOp0YTjAMBggqgRzPVQGDdQUAMGExCzAJBgNVBAYMAkNOMQ0wCwYDVQQKDARCSkNBMSUwIwYDVQQLDBxCSkNBIEFueXdyaXRlIFRydXN0IFNlcnZpY2VzMRwwGgYDVQQDDBNUcnVzdC1TaWduIFNNMiBDQS0xMB4XDTIwMDgxMzIwMTkzNFoXDTIwMTAyNDE1NTk1OVowHjELMAkGA1UEBgwCQ04xDzANBgNVBAMMBuWGr+i9rDBZMBMGByqGSM49AgEGCCqBHM9VAYItA0IABAIF97Sqq0Rv616L2PjFP3xt16QGJLmi+W8Ht+NLHiXntgUey0Nz+ZVnSUKUMzkKuGTikY3h2v7la20b6lpKo8WjgZIwgY8wCwYDVR0PBAQDAgbAMB0GA1UdDgQWBBSxiaS6z4Uguz3MepS2zblkuAF/LTAfBgNVHSMEGDAWgBTMZyRCGsP4rSes0vLlhIEf6cUvrjBABgNVHSAEOTA3MDUGCSqBHIbvMgICAjAoMCYGCCsGAQUFBwIBFhpodHRwOi8vd3d3LmJqY2Eub3JnLmNuL2NwczAMBggqgRzPVQGDdQUAA0gAMEUCIG6n6PG0BOK1EdFcvetQlC+9QhpsTuTui2wkeqWiPKYWAiEAvqR8Z+tSiYR5DIs7SyHJPWZ+sa8brtQL/1jURvHGxU8="
+	input.Text = "MIICETCCAbWgAwIBAgINKl81oFaaablKOp0YTjAMBggqgRzPVQGDdQUAMGExCzAJBgNVBAYMAkNOMQ0wCwYDVQQKDARCSkNBMSUwIwYDVQQLDBxCSkNBIEFueXdyaXRlIFRydXN0IFNlcnZpY2VzMRwwGgYDVQQDDBNUcnVzdC1TaWduIFNNMiBDQS0xMB4XDTIwMDgxMzIwMTkzNFoXDTIwMTAyNDE1NTk1OVowHjELMAkGA1UEBgwCQ04xDzANBgNVBAMMBuWGr+i9rDBZMBMGByqGSM49AgEGCCqBHM9VAYItA0IABAIF97Sqq0Rv616L2PjFP3xt16QGJLmi+W8Ht+NLHiXntgUey0Nz+ZVnSUKUMzkKuGTikY3h2v7la20b6lpKo8WjgZIwgY8wCwYDVR0PBAQDAgbAMB0GA1UdDgQWBBSxiaS6z4Uguz3MepS2zblkuAF/LTAfBgNVHSMEGDAWgBTMZyRCGsP4rSes0vLlhIEf6cUvrjBABgNVHSAEOTA3MDUGCSqBHIbvMgICAjAoMCYGCCsGAQUFBwIBFhpodHRwOi8vd3d3LmJqY2Eub3JnLmNuL2NwczAMBggqgRzPVQGDdQUAA0gAMEUCIG6n6PG0BOK1EdFcvetQlC+9QhpsTuTui2wkeqWiPKYWAiEAvqR8Z+tSiYR5DIs7SyHJPWZ+sa8brtQL/1jURvHGxU8="
 
 	// Tree数据存储
 	treeData := make(map[string]ASN1Node)
@@ -93,4 +94,35 @@ func Asn1Structure() *fyne.Container {
 
 	return container.NewBorder(vbox, nil, nil, nil, accordion)
 
+}
+
+func getRealTagName(tag int) string {
+	prefix := ""
+	//32 = 0x20, ASN1中小于0x20的都是通用简单类型
+
+	//0x20 到 0x40 通用,结构类型
+	if 32 <= tag && tag < 64 {
+		//prefix = "Universal Structure "
+		tag -= 32
+	} else if 64 <= tag && tag < 96 {
+		prefix = "Application Simple "
+		tag -= 64
+	} else if 96 <= tag && tag < 128 {
+		prefix = "Application Structure "
+		tag -= 96
+	} else if 128 <= tag && tag < 160 {
+		prefix = "Context Specific Simple "
+		tag -= 128
+	} else if 160 <= tag && tag < 192 {
+		prefix = "Context Specific Structure "
+		tag -= 160
+	} else if 192 <= tag && tag < 224 {
+		prefix = "Private Simple "
+		tag -= 192
+	} else if 224 <= tag && tag < 256 {
+		prefix = "Private Structure "
+		tag -= 224
+	}
+
+	return prefix + TagToName[tag]
 }
