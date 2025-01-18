1031588c0647151fd3736e0a30bc79e96543927d
zaneway <zaneway1108@gmail.com>
2025-01-18 23:03:55 +0800

    完善解析方式

--- window/coder.go
+++ window/coder.go
@@ -3,6 +3,7 @@ package window
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
@@ -17,6 +18,7 @@ func CoderStructure() *fyne.Container {
 	output := widget.NewMultiLineEntry()
 	output.Hide()
 	input.SetPlaceHolder("Please input base64/hex data")
+	dataLenPrint := widget.NewLabel("")
 	// 解析按钮
 	confirmButton := widget.NewButtonWithIcon("确认", theme.ConfirmIcon(), func() {
 		inputData := strings.TrimSpace(input.Text)
@@ -25,22 +27,33 @@ func CoderStructure() *fyne.Container {
 		if err == nil {
 			output.Text = base64.StdEncoding.EncodeToString(decodedData)
 		} else {
-			hexDecodeString, _ := base64.StdEncoding.DecodeString(inputData)
-			output.Text = hex.EncodeToString(hexDecodeString)
+			decodedData, err = base64.StdEncoding.DecodeString(inputData)
+			if err == nil {
+				output.Text = hex.EncodeToString(decodedData)
+			} else {
+				decodedData = []byte(inputData)
+				output.Text = hex.EncodeToString(decodedData)
+			}
 		}
+		dataLen := len(decodedData)
+		dataLenPrint.Text = fmt.Sprintf("%s%d%s", "数据长度为:", dataLen, "字节(优先按照HEX解析,后按照Base64解析,普通字符串兜底)")
+
+		dataLenPrint.Refresh()
 		output.Show()
 		output.Refresh()
 	})
+
 	//清除按钮
 	cancelButton := buildButton("清除", theme.CancelIcon(), func() {
 		input.Text = ""
 		output.Text = ""
+		dataLenPrint.Text = ""
 		input.Refresh()
 		output.Refresh()
 	})
 	// 布局
 	allButton := container.New(layout.NewGridLayout(2), confirmButton, cancelButton)
-	vbox := container.NewVBox(input, allButton, output)
+	vbox := container.NewVBox(input, allButton, dataLenPrint, output)
 
 	return vbox
 
