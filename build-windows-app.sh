5c2135840b4c3e13084724497cf054548749c293
zaneway <zaneway1108@gmail.com>
2025-03-07 14:10:43 +0800

    update app name

--- build-windows-app.sh
+++ build-windows-app.sh
@@ -2,9 +2,9 @@
 # 需要安装 mingw-w64
 dir=$(pwd)
 go mod tidy
-echo 准备删除 $dir/CertViewer.exe
-rm -f $dir/CertViewer.exe
+echo 准备删除 $dir/HeTu.exe
+rm -f $dir/HeTu.exe
 echo 删除完成,开始打包
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -ldflags "-H windowsgui" -o CertViewer.exe
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc go build -ldflags "-H windowsgui" -o HeTu.exe
 
 echo over
