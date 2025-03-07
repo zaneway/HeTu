4356ae0730ca3252410c366a9699a3e5f3934479
zaneway <zaneway1108@gmail.com>
2025-03-07 09:42:56 +0800

    补充密钥对实现

--- gm/GM.go
+++ gm/GM.go
@@ -4,7 +4,6 @@ import (
 	"encoding/asn1"
 	"github.com/zaneway/cain-go/sm2"
 	"github.com/zaneway/cain-go/sm4"
-	"github.com/zaneway/cain-go/x509"
 	"math/big"
 )
 
@@ -13,11 +12,6 @@ type SM2Cipher struct {
 	Y          *big.Int `asn1:"integer"`
 	Hash       []byte
 	CipherText []byte
-}
-
-type KeyPair struct {
-	PublicKey  *sm2.PublicKey
-	PrivateKey *sm2.PrivateKey
 }
 
 type SM2EnvelopedKey struct {
@@ -37,20 +31,5 @@ func DecryptDataUsePrivateKey(data []byte, privateKey *sm2.PrivateKey) ([]byte, 
 }
 
 func DecryptDataUseSm4Key(data []byte, key []byte) (out []byte, err error) {
-	return sm4.Sm4Ecb(key, data, false)
-}
-
-func BuildKeyPair(publicKey []byte, privateKey []byte) (*KeyPair, error) {
-	sm2PrivateKey, err := x509.ParseSm2PrivateKey(privateKey)
-	if err != nil {
-		return nil, err
-	}
-	sm2PublicKey, err := x509.ParseSm2PublicKey(publicKey)
-	if err != nil {
-		return nil, err
-	}
-	return &KeyPair{
-		PrivateKey: sm2PrivateKey,
-		PublicKey:  sm2PublicKey,
-	}, nil
+	return sm4.Sm4EcbNoPaddingCipher(key, data, false)
 }
